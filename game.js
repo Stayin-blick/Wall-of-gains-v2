de142f7aacb8a63546dae9bd830a5bc487e354d5
Stayin_blick <azeezbello75@gmail.com>
2022-11-09 01:00:03 +0000

    ...

---
+++ game.js
@@ -0,0 +1,10 @@
+let heads = 0;
+let tails = 1;
+let coin = document.querySelector(".coin");
+let flipCoin = document.querySelector("#flip-button");
+
+flipCoin.addEventListener("click", my function);
+/* run function if radio is selected, with function result check for === with user pick if true add one to score*/
+function userPick {
+    if ()
+}
