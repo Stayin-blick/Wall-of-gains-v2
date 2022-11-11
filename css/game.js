00461b22410d924a4086c40db045f6204a6a2425
Stayin_blick <azeezbello75@gmail.com>
2022-11-11 01:17:31 +0000

    ...

--- css/game.js
+++ css/game.js
@@ -1,6 +1,18 @@
 let heads = 0;
 let tails = 0;
-let coin = document.querySelector(".coin");
-let flipCoin = document.querySelector("#flip-button");
+let coin = document.getElementById('coin');
+let flipCoin= document.getElementById('flip-button');
+let result = document.getElementById('result');
+let options = document.querySelectorAll('button');
+let userChoice ='';
 
-console.log(coin flipCoin);
+
+
+
+/* snake script */
+
+function main (currentTime) {
+    window.requestAnimationFrame(main);
+    console.log (currentTime);
+}
+window.requestAnimationFrame(main)
