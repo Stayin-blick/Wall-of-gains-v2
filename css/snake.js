3d157b0821dcd180ef86b7fdcc8e0734ef0e59de
Stayin_blick <azeezbello75@gmail.com>
2022-11-11 04:16:20 +0000

    ...

--- css/snake.js
+++ css/snake.js
@@ -1,25 +1,25 @@
+import { getInputDirection } from "./snake-input.js";
+
 export const snakeSpeed = 1; 
-const snakeBody = [
-    { x:11, y:11},
-    { x:12, y:11},
-    {x:13, y:12} 
-]
+const snakeBody = [{ x:11, y:11}]
 
 export function update () {
-    for (let i = snakeBody.length -2; i>= 0; i--) {
-        snakeBody [i+1]} = {snakeBody[i]}
-}
+    const inputDirection = getInputDirection()
+    for (let i = snakeBody.length - 2; i>= 0; i--) {
+        snakeBody [i + 1] = {...snakeBody[i]}
+    }
 
-//snakeBody[0].x += 0
-snakeBody[0].y += 1//
+snakeBody[0].x += inputDirection.x
+snakeBody[0].y += inputDirection.y
+}
 
 
 export function draw(gameBoard) {
-    snakeBody.forEach(segment => {
+     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div')
         snakeElement.style.gridRowStart = segment.y
         snakeElement.style.gridColumnStart = segment.x
         snakeElement.classList.add('snake')
         gameBoard.appendChild (snakeElement)
-    })
-}
+     })
+    }
